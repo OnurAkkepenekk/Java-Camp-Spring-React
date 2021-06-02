0259a4549c39d2942ac01fc90c22685d5a9aae80
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-02 23:07:51 +0300

    Create result structure

---
+++ Northwind/src/main/java/kodlamaio/Northwind/core/utilities/results/ErrorResult.java
@@ -0,0 +1,13 @@
+package kodlamaio.Northwind.core.utilities.results;
+
+public class ErrorResult extends Result {
+	public ErrorResult() {
+		super(false);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ErrorResult(String message) {
+		super(false, message);
+		// TODO Auto-generated constructor stub
+	}
+}
