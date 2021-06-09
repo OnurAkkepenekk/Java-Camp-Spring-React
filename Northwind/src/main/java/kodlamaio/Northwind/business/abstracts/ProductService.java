305039ca68ada4d7236599a6ec8e1628974e038a
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-10 02:44:21 +0300

    Update ProductService.java

--- Northwind/src/main/java/kodlamaio/Northwind/business/abstracts/ProductService.java
+++ Northwind/src/main/java/kodlamaio/Northwind/business/abstracts/ProductService.java
@@ -2,11 +2,28 @@ package kodlamaio.Northwind.business.abstracts;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+
 import kodlamaio.Northwind.core.utilities.results.DataResult;
 import kodlamaio.Northwind.core.utilities.results.Result;
 import kodlamaio.Northwind.entities.concretes.Product;
 
 public interface ProductService {
 	DataResult<List<Product>> getAll();
+
 	Result add(Product product);
+
+	DataResult<Product> getByProductName(String productName);
+
+	DataResult<Product> getByProductNameAndCategory(String productName, String categoryId);
+
+	DataResult<List<Product>> getByProductNameOrCategory(String productName, String categoryId);
+
+	DataResult<List<Product>> getByCategoryIn(List<Integer> categories);
+
+	DataResult<List<Product>> getByProductNameContains(String productName);
+
+	DataResult<List<Product>> getByProductNameStartWith(String productName);
+
+	DataResult<List<Product>> getByNameAndCategory(String productName, int category);
 }
