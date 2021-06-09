7757a919b603e1da7aa6be3026b0c846b722eb04
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-10 02:44:26 +0300

    Update ProductDao.java

--- Northwind/src/main/java/kodlamaio/Northwind/dataAccess/abstracts/ProductDao.java
+++ Northwind/src/main/java/kodlamaio/Northwind/dataAccess/abstracts/ProductDao.java
@@ -1,9 +1,25 @@
 package kodlamaio.Northwind.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import kodlamaio.Northwind.entities.concretes.Product;
 
 public interface ProductDao extends JpaRepository<Product, Integer> {
+	Product getByProductName(String productName);
 
+	Product getByProductNameAndCategory(String productName, String categoryId);
+
+	List<Product> getByProductNameOrCategory(String productName, String categoryId);
+
+	List<Product> getByCategoryIn(List<Integer> categories);
+
+	List<Product> getByProductNameContains(String productName);
+
+	List<Product> getByProductNameStartsWith(String productName);
+
+	@Query("From Product where ProductName=:productName and category.categoryId=:categoryId")
+	List<Product> getByNameAndCategory(String productName, int category);
 }
