393c653d8e2572e7e9bf474f49a6619303ce4c1e
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-10 16:19:42 +0300

    Update Product manager and service

--- Northwind/src/main/java/kodlamaio/Northwind/business/abstracts/ProductService.java
+++ Northwind/src/main/java/kodlamaio/Northwind/business/abstracts/ProductService.java
@@ -10,16 +10,19 @@ import kodlamaio.Northwind.entities.concretes.Product;
 
 public interface ProductService {
 	DataResult<List<Product>> getAll();
+	DataResult<List<Product>> getAllSorted();
+
+	DataResult<List<Product>> getAll(int pageNo, int pageSize);
 
 	Result add(Product product);
 
 	DataResult<Product> getByProductName(String productName);
 
-	DataResult<Product> getByProductNameAndCategory(String productName, String categoryId);
+	DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId);
 
-	DataResult<List<Product>> getByProductNameOrCategory(String productName, String categoryId);
+	DataResult<List<Product>> getByProductNameOrCategoryId(String productName, String categoryId);
 
-	DataResult<List<Product>> getByCategoryIn(List<Integer> categories);
+	DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);
 
 	DataResult<List<Product>> getByProductNameContains(String productName);
 
