8c67d5d1fff8753807c0d3c1e6ece29095c5b2c2
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-17 13:35:09 +0300

    Create user manager

---
+++ Northwind/src/main/java/kodlamaio/Northwind/business/concretes/UserManager.java
@@ -0,0 +1,36 @@
+package kodlamaio.Northwind.business.concretes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.Northwind.business.abstracts.UserService;
+import kodlamaio.Northwind.core.dataAccess.UserDao;
+import kodlamaio.Northwind.core.entities.User;
+import kodlamaio.Northwind.core.utilities.results.DataResult;
+import kodlamaio.Northwind.core.utilities.results.Result;
+import kodlamaio.Northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.Northwind.core.utilities.results.SuccessResult;
+
+@Service
+public class UserManager implements UserService {
+
+	private UserDao userDao;
+
+	@Autowired
+	public UserManager(UserDao userDao) {
+		super();
+		this.userDao = userDao;
+	}
+
+	@Override
+	public Result add(User user) {
+		this.userDao.save(user);
+		return new SuccessResult("User added");
+	}
+
+	@Override
+	public DataResult<User> findByEmail(String email) {
+		return new SuccessDataResult<User>(this.userDao.findByEmail(email), "User find");
+	}
+
+}
