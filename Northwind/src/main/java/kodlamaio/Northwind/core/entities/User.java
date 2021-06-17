4054e02ec34c1ffe7017c46dd4b6bb9b8497090e
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-17 13:34:32 +0300

    Create User entity

---
+++ Northwind/src/main/java/kodlamaio/Northwind/core/entities/User.java
@@ -0,0 +1,38 @@
+package kodlamaio.Northwind.core.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "email")
+	@Email
+	@NotNull
+	@NotBlank
+	private String email;
+
+	@Column(name = "password")
+	@NotNull
+	@NotBlank
+	private String password;
+}
